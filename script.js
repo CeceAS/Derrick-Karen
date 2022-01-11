159b5c7682d952dda8f9728a76007cd3181e8df0
Cece <cecesanclemente@gmail.com>
2022-01-11 11:46:33 -0600

    Add section reveal animation on scroll

--- script.js
+++ script.js
@@ -47,3 +47,25 @@ navButtonsContainer.addEventListener("click", function (e) {
   const clicked = e.target.getAttribute("href");
   document.querySelector(clicked)?.scrollIntoView({ behavior: "smooth" });
 });
+
+// UNIT: Animation when sections enter viewport:
+
+const allSections = document.querySelectorAll("section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section-hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section-hidden");
+});
